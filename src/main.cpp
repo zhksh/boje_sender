1534e30d539beaeb7e32d9066fa79bdc32b16182
zhksh <zhksh@ncmg.eu>
2024-03-29 20:24:20 +0100

    working, gps now

--- src/main.cpp
+++ src/main.cpp
@@ -124,6 +124,8 @@ TinyGsmClient client(modem, 0);
 HttpClient    http(client, server, port);
 
 void setup() {
+
+  int start = millis();
   // Set console baud rate
   SerialMon.begin(115200);
   delay(10);
@@ -139,20 +141,92 @@ void setup() {
   // TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
   SerialAT.begin(115200);
   SerialRX.begin(9600);
+
+  DBG("Initializing modem...");
+  bool modem_running = modem.restart();
+  while (!modem_running){
+    // if (!modem.init()) {
+    DBG("Failed to restart modem, delaying 10s and retrying");
+    modem_running = modem.restart();
+    // restart autobaud in case GSM just rebooted
+    // TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
+  }
+
+  String name = modem.getModemName();
+  DBG("Modem Name:", name);
+
+  String modemInfo = modem.getModemInfo();
+  DBG("Modem Info:", modemInfo);
+
+  #if TINY_GSM_TEST_GPRS
+    // Unlock your SIM card with a PIN if needed
+    if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
+  #endif
+
+
+// #if TINY_GSM_TEST_GPRS && defined TINY_GSM_MODEM_XBEE
+//   // The XBee must run the gprsConnect function BEFORE waiting for network!
+//   modem.gprsConnect(apn, gprsUser, gprsPass);
+// #endif
+  // bool network_connected = 
+  DBG("Waiting for network...");
+  if (!modem.waitForNetwork(600000L, true)) {
+    delay(10000);
+    return;
+  }
+
+  if (modem.isNetworkConnected()) { DBG("Network connected"); }
+
+  #if TINY_GSM_TEST_GPRS
+    DBG("Connecting to", apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+      delay(10000);
+      return;
+    }
+
+    bool res = modem.isGprsConnected();
+    DBG("GPRS status:", res ? "connected" : "not connected");
+
+    String ccid = modem.getSimCCID();
+    DBG("CCID:", ccid);
+
+    String imei = modem.getIMEI();
+    DBG("IMEI:", imei);
+
+    String imsi = modem.getIMSI();
+    DBG("IMSI:", imsi);
+
+    String cop = modem.getOperator();
+    DBG("Operator:", cop);
+
+    IPAddress local = modem.localIP();
+    DBG("Local IP:", local);
+
+    int csq = modem.getSignalQuality();
+    DBG("Signal quality:", csq);
+
+    DBG("Enabling GPS/GNSS/GLONASS and waiting 15s for warm-up");
+    modem.enableGPS();
+    delay(15000L);
+
+    DBG("Startup took:");
+    DBG(millis()-start);
+
+  #endif
 }
 
 String createRandData(){
   return "{\"temp\": \"" + String(random(10, 200)) + " \", \"pressure\": \"" + String(random(5000, 30000)/100) + " \", \"depth\": \"" + String(random(0, 100)/10) + " \", \"env_dist\": \"" + String(random(0, 20)/10) + " \", \"gps\": \"12.39059306, 6.79417558\", \"ts\": \"" + String(millis()/1000) + "\"}";
 }
 
-void post(HttpClient& http, const String& content, int c)
+void post(HttpClient& http, const String& data)
 {
   SerialMon.println("making POST request");
   String contentType = "application/json";
-  String data = content + ": " +  String(c);
-  String postData = createRandData();
-
-  http.post(resource, contentType, postData);
+  // String postData = createRandData();
+  SerialMon.print("Data: ");
+  SerialMon.println(data);
+  http.post(resource, contentType, data);
 
   // read the status code and body of the response
   int status = http.responseStatusCode();
@@ -186,9 +260,35 @@ void post(HttpClient& http, const String& content, int c)
 
 
 
-void loop() {
 
-    if (SerialRX.available()) {
+void loop() {
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  
+  //  String gps_raw = modem.getGPSraw();
+    DBG("GPS/GNSS Based Location String:");
+      float lat2      = 0;
+      float lon2      = 0;
+      float speed2    = 0;
+      float alt2      = 0;
+      int   vsat2     = 0;
+      int   usat2     = 0;
+      float accuracy2 = 0;
+      int   year2     = 0;
+      int   month2    = 0;
+      int   day2      = 0;
+      int   hour2     = 0;
+      int   min2      = 0;
+      int   sec2      = 0;
+    modem.getGPS(&lat2, &lon2, &speed2, &alt2, &vsat2, &usat2, &accuracy2,&year2, &month2, &day2, &hour2, &min2, &sec2);
+      DBG("Latitude:", String(lat2, 8), "\tLongitude:", String(lon2, 8));
+      DBG("Speed:", speed2, "\tAltitude:", alt2);
+      DBG("Visible Satellites:", vsat2, "\tUsed Satellites:", usat2);
+      DBG("Accuracy:", accuracy2);
+      DBG("Year:", year2, "\tMonth:", month2, "\tDay:", day2);
+      DBG("Hour:", hour2, "\tMinute:", min2, "\tSecond:", sec2);
+    return;
+  if (SerialRX.available()) {
     // Allocate the JSON document
     // This one must be bigger than the sender's because it must store the strings
     StaticJsonDocument<300> data;
@@ -196,22 +296,27 @@ void loop() {
     // Read the JSON document from the "link" serial port
     DeserializationError err = deserializeJson(data, SerialRX);
 
-    if (err == DeserializationError::Ok) 
-    {
-      data["gps"] = "frank";
+    if (err == DeserializationError::Ok) {
+      String gps_raw = modem.getGPSraw();
+      DBG("GPS/GNSS Based Location String:", gps_raw);
+      data["gps"] = "11.57960005323633, 48.12800085254037";
+      data["gps"] = gps_raw;
+      // data["ts"] = millis()/1000;
+
       String payload;
       serializeJson(data, payload);
-      Serial.println(payload);
+      // DBG(payload);
+      post(http, payload);
+      // client.stop();
 
-      Serial.print("timestamp = ");
-      Serial.println(data["temp"].as<float>());
-      Serial.print("altitude = ");
-      Serial.println(data["altitude"].as<float>());
-      Serial.print("pressure = ");
-      Serial.println(data["pressure"].as<float>());
+      // Serial.print("timestamp = ");
+      // Serial.println(data["temp"].as<float>());
+      // Serial.print("altitude = ");
+      // Serial.println(data["altitude"].as<float>());
+      // Serial.print("pressure = ");
+      // Serial.println(data["pressure"].as<float>());
     } 
-    else 
-    {
+    else {
       // Print error to the "debug" serial port
       Serial.print("deserializeJson() returned ");
       Serial.println(err.c_str());
@@ -221,270 +326,117 @@ void loop() {
         SerialRX.read();
     }
   }
-    return;
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  DBG("Initializing modem...");
-  if (!modem.restart()) {
-    // if (!modem.init()) {
-    DBG("Failed to restart modem, delaying 10s and retrying");
-    // restart autobaud in case GSM just rebooted
-    // TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
-    return;
-  }
-
-  String name = modem.getModemName();
-  DBG("Modem Name:", name);
-
-  String modemInfo = modem.getModemInfo();
-  DBG("Modem Info:", modemInfo);
-
-#if TINY_GSM_TEST_GPRS
-  // Unlock your SIM card with a PIN if needed
-  if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
-#endif
+  // int c = 0;
+  // while (true && c < 200){
+  //   DBG("reading from serial");
+  //   while (SerialRX.available() > 0) {
+  //     String data = SerialRX.readString();
+  //     DBG(data);
+  //   post(http, "hi", c++);
+  //   client.stop();
+  // }
 
 
-// #if TINY_GSM_TEST_GPRS && defined TINY_GSM_MODEM_XBEE
-//   // The XBee must run the gprsConnect function BEFORE waiting for network!
-//   modem.gprsConnect(apn, gprsUser, gprsPass);
-// #endif
-
-  DBG("Waiting for network...");
-  if (!modem.waitForNetwork(600000L, true)) {
-    delay(10000);
-    return;
-  }
-
-  if (modem.isNetworkConnected()) { DBG("Network connected"); }
-
-#if TINY_GSM_TEST_GPRS
-  DBG("Connecting to", apn);
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    delay(10000);
-    return;
-  }
-
-  bool res = modem.isGprsConnected();
-  DBG("GPRS status:", res ? "connected" : "not connected");
-
-  String ccid = modem.getSimCCID();
-  DBG("CCID:", ccid);
-
-  String imei = modem.getIMEI();
-  DBG("IMEI:", imei);
-
-  String imsi = modem.getIMSI();
-  DBG("IMSI:", imsi);
-
-  String cop = modem.getOperator();
-  DBG("Operator:", cop);
-
-  IPAddress local = modem.localIP();
-  DBG("Local IP:", local);
-
-  int csq = modem.getSignalQuality();
-  DBG("Signal quality:", csq);
-#endif
-
-// #if TINY_GSM_TEST_USSD && defined TINY_GSM_MODEM_HAS_SMS
-//   String ussd_balance = modem.sendUSSD("*111#");
-//   DBG("Balance (USSD):", ussd_balance);
-
-//   String ussd_phone_num = modem.sendUSSD("*161#");
-//   DBG("Phone number (USSD):", ussd_phone_num);
-// #endif
-
-#if TINY_GSM_TEST_TCP && defined TINY_GSM_MODEM_HAS_TCP
 
 
-  int c = 0;
-  while (true && c < 200){
-    DBG("reading from serial");
-    while (SerialRX.available() > 0) {
-      String data = SerialRX.readString();
-      DBG(data);
-    post(http, "hi", c++);
-    client.stop();
-
-  }
-
-#endif
-
-#if TINY_GSM_TEST_SSL && defined TINY_GSM_MODEM_HAS_SSL
-  TinyGsmClientSecure secureClient(modem, 1);
-  const int           securePort = 443;
-  DBG("Connecting securely to", server);
-  if (!secureClient.connect(server, securePort)) {
-    DBG("... failed");
-  } else {
-    // Make a HTTP GET request:
-    secureClient.print(String("GET ") + resource + " HTTP/1.0\r\n");
-    secureClient.print(String("Host: ") + server + "\r\n");
-    secureClient.print("Connection: close\r\n\r\n");
-
-    // Wait for data to arrive
-    uint32_t startS = millis();
-    while (secureClient.connected() && !secureClient.available() &&
-           millis() - startS < 30000L) {
-      delay(100);
-    };
-
-    // Read data
-    startS          = millis();
-    char logoS[640] = {
-        '\0',
-    };
-    int read_charsS = 0;
-    while (secureClient.connected() && millis() - startS < 10000L) {
-      while (secureClient.available()) {
-        logoS[read_charsS]     = secureClient.read();
-        logoS[read_charsS + 1] = '\0';
-        read_charsS++;
-        startS = millis();
-      }
-    }
-    SerialMon.println(logoS);
-    DBG("#####  RECEIVED:", strlen(logoS), "CHARACTERS");
-    secureClient.stop();
-  }
-#endif
-
-
-#if TINY_GSM_TEST_GSM_LOCATION && defined TINY_GSM_MODEM_HAS_GSM_LOCATION
-  float lat      = 0;
-  float lon      = 0;
-  float accuracy = 0;
-  int   year     = 0;
-  int   month    = 0;
-  int   day      = 0;
-  int   hour     = 0;
-  int   min      = 0;
-  int   sec      = 0;
-  for (int8_t i = 15; i; i--) {
-    DBG("Requesting current GSM location");
-    if (modem.getGsmLocation(&lat, &lon, &accuracy, &year, &month, &day, &hour,
-                             &min, &sec)) {
-      DBG("Latitude:", String(lat, 8), "\tLongitude:", String(lon, 8));
-      DBG("Accuracy:", accuracy);
-      DBG("Year:", year, "\tMonth:", month, "\tDay:", day);
-      DBG("Hour:", hour, "\tMinute:", min, "\tSecond:", sec);
-      break;
-    } else {
-      DBG("Couldn't get GSM location, retrying in 15s.");
-      delay(15000L);
-    }
-  }
-  DBG("Retrieving GSM location again as a string");
-  String location = modem.getGsmLocation();
-  DBG("GSM Based Location String:", location);
-#endif
-
-#if TINY_GSM_TEST_GPS && defined TINY_GSM_MODEM_HAS_GPS
-  DBG("Enabling GPS/GNSS/GLONASS and waiting 15s for warm-up");
-  modem.enableGPS();
-  delay(15000L);
-  float lat2      = 0;
-  float lon2      = 0;
-  float speed2    = 0;
-  float alt2      = 0;
-  int   vsat2     = 0;
-  int   usat2     = 0;
-  float accuracy2 = 0;
-  int   year2     = 0;
-  int   month2    = 0;
-  int   day2      = 0;
-  int   hour2     = 0;
-  int   min2      = 0;
-  int   sec2      = 0;
-  for (int8_t i = 15; i; i--) {
-    DBG("Requesting current GPS/GNSS/GLONASS location");
-    if (modem.getGPS(&lat2, &lon2, &speed2, &alt2, &vsat2, &usat2, &accuracy2,
-                     &year2, &month2, &day2, &hour2, &min2, &sec2)) {
-      DBG("Latitude:", String(lat2, 8), "\tLongitude:", String(lon2, 8));
-      DBG("Speed:", speed2, "\tAltitude:", alt2);
-      DBG("Visible Satellites:", vsat2, "\tUsed Satellites:", usat2);
-      DBG("Accuracy:", accuracy2);
-      DBG("Year:", year2, "\tMonth:", month2, "\tDay:", day2);
-      DBG("Hour:", hour2, "\tMinute:", min2, "\tSecond:", sec2);
-      break;
-    } else {
-      DBG("Couldn't get GPS/GNSS/GLONASS location, retrying in 15s.");
-      delay(15000L);
-    }
-  }
-  DBG("Retrieving GPS/GNSS/GLONASS location again as a string");
-  String gps_raw = modem.getGPSraw();
-  DBG("GPS/GNSS Based Location String:", gps_raw);
-  DBG("Disabling GPS");
-  modem.disableGPS();
-#endif
-
-// #if TINY_GSM_TEST_NTP && defined TINY_GSM_MODEM_HAS_NTP
-//   DBG("Asking modem to sync with NTP");
-//   modem.NTPServerSync("132.163.96.5", 20);
-// #endif
-
-// #if TINY_GSM_TEST_TIME && defined TINY_GSM_MODEM_HAS_TIME
-//   int   year3    = 0;
-//   int   month3   = 0;
-//   int   day3     = 0;
-//   int   hour3    = 0;
-//   int   min3     = 0;
-//   int   sec3     = 0;
-//   float timezone = 0;
-//   for (int8_t i = 5; i; i--) {
-//     DBG("Requesting current network time");
-//     if (modem.getNetworkTime(&year3, &month3, &day3, &hour3, &min3, &sec3,
-//                              &timezone)) {
-//       DBG("Year:", year3, "\tMonth:", month3, "\tDay:", day3);
-//       DBG("Hour:", hour3, "\tMinute:", min3, "\tSecond:", sec3);
-//       DBG("Timezone:", timezone);
+// #if TINY_GSM_TEST_GSM_LOCATION && defined TINY_GSM_MODEM_HAS_GSM_LOCATION
+//   float lat      = 0;
+//   float lon      = 0;
+//   float accuracy = 0;
+//   int   year     = 0;
+//   int   month    = 0;
+//   int   day      = 0;
+//   int   hour     = 0;
+//   int   min      = 0;
+//   int   sec      = 0;
+//   for (int8_t i = 15; i; i--) {
+//     DBG("Requesting current GSM location");
+//     if (modem.getGsmLocation(&lat, &lon, &accuracy, &year, &month, &day, &hour,
+//                              &min, &sec)) {
+//       DBG("Latitude:", String(lat, 8), "\tLongitude:", String(lon, 8));
+//       DBG("Accuracy:", accuracy);
+//       DBG("Year:", year, "\tMonth:", month, "\tDay:", day);
+//       DBG("Hour:", hour, "\tMinute:", min, "\tSecond:", sec);
 //       break;
 //     } else {
-//       DBG("Couldn't get network time, retrying in 15s.");
+//       DBG("Couldn't get GSM location, retrying in 15s.");
 //       delay(15000L);
 //     }
 //   }
-//   DBG("Retrieving time again as a string");
-//   String time = modem.getGSMDateTime(DATE_FULL);
-//   DBG("Current Network Time:", time);
+//   DBG("Retrieving GSM location again as a string");
+//   String location = modem.getGsmLocation();
+//   DBG("GSM Based Location String:", location);
+// #endif
+
+// #if TINY_GSM_TEST_GPS && defined TINY_GSM_MODEM_HAS_GPS
+//   DBG("Enabling GPS/GNSS/GLONASS and waiting 15s for warm-up");
+//   modem.enableGPS();
+//   delay(15000L);
+//   float lat2      = 0;
+//   float lon2      = 0;
+//   float speed2    = 0;
+//   float alt2      = 0;
+//   int   vsat2     = 0;
+//   int   usat2     = 0;
+//   float accuracy2 = 0;
+//   int   year2     = 0;
+//   int   month2    = 0;
+//   int   day2      = 0;
+//   int   hour2     = 0;
+//   int   min2      = 0;
+//   int   sec2      = 0;
+//   for (int8_t i = 15; i; i--) {
+//     DBG("Requesting current GPS/GNSS/GLONASS location");
+//     if (modem.getGPS(&lat2, &lon2, &speed2, &alt2, &vsat2, &usat2, &accuracy2,
+//                      &year2, &month2, &day2, &hour2, &min2, &sec2)) {
+//       DBG("Latitude:", String(lat2, 8), "\tLongitude:", String(lon2, 8));
+//       DBG("Speed:", speed2, "\tAltitude:", alt2);
+//       DBG("Visible Satellites:", vsat2, "\tUsed Satellites:", usat2);
+//       DBG("Accuracy:", accuracy2);
+//       DBG("Year:", year2, "\tMonth:", month2, "\tDay:", day2);
+//       DBG("Hour:", hour2, "\tMinute:", min2, "\tSecond:", sec2);
+//       break;
+//     } else {
+//       DBG("Couldn't get GPS/GNSS/GLONASS location, retrying in 15s.");
+//       delay(15000L);
+//     }
+//   }
+//   DBG("Retrieving GPS/GNSS/GLONASS location again as a string");
+//   String gps_raw = modem.getGPSraw();
+//   DBG("GPS/GNSS Based Location String:", gps_raw);
+//   DBG("Disabling GPS");
+//   modem.disableGPS();
 // #endif
 
 
 
-#if TINY_GSM_TEST_TEMPERATURE && defined TINY_GSM_MODEM_HAS_TEMPERATURE
-  float temp = modem.getTemperature();
-  DBG("Chip temperature:", temp);
-#endif
+// #if TINY_GSM_POWERDOWN
 
-#if TINY_GSM_POWERDOWN
+// #if TINY_GSM_TEST_GPRS
+//   modem.gprsDisconnect();
+//   delay(5000L);
+//   if (!modem.isGprsConnected()) {
+//     DBG("GPRS disconnected");
+//   } else {
+//     DBG("GPRS disconnect: Failed.");
+//   }
+// #endif
 
-#if TINY_GSM_TEST_GPRS
-  modem.gprsDisconnect();
-  delay(5000L);
-  if (!modem.isGprsConnected()) {
-    DBG("GPRS disconnected");
-  } else {
-    DBG("GPRS disconnect: Failed.");
-  }
-#endif
+// #if TINY_GSM_TEST_WIFI
+//   modem.networkDisconnect();
+//   DBG("WiFi disconnected");
+// #endif
 
-#if TINY_GSM_TEST_WIFI
-  modem.networkDisconnect();
-  DBG("WiFi disconnected");
-#endif
+//   // Try to power-off (modem may decide to restart automatically)
+//   // To turn off modem completely, please use Reset/Enable pins
+//   modem.poweroff();
+//   DBG("Poweroff.");
+// #endif
 
-  // Try to power-off (modem may decide to restart automatically)
-  // To turn off modem completely, please use Reset/Enable pins
-  modem.poweroff();
-  DBG("Poweroff.");
-#endif
-
-  DBG("End of tests.");
+//   DBG("End of tests.");
 
   // Do nothing forevermore
-  while (true) { modem.maintain(); }
+  // while (true) { modem.maintain(); }
 
-  }
+  
 }
